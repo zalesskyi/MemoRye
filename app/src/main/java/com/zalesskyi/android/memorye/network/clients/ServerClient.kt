55f3497e90cb53d5525681edc0a7126ee37d6547
alexeyzalesskiy <alexeyzalesskiy08@gmail.com>
2019-06-30 10:38:25 +0300

    Done. Connected Lyrics API.

--- app/src/main/java/com/zalesskyi/android/memorye/network/clients/ServerClient.kt
+++ app/src/main/java/com/zalesskyi/android/memorye/network/clients/ServerClient.kt
@@ -6,6 +6,7 @@ import com.zalesskyi.android.memorye.BuildConfig
 import com.zalesskyi.android.memorye.network.api.LyricsApi
 import com.zalesskyi.android.memorye.network.api.TranslatorApi
 import com.zalesskyi.android.memorye.network.modules.LyricsModuleImpl
+import com.zalesskyi.android.memorye.network.modules.NetworkModule.mapper
 import com.zalesskyi.android.memorye.network.modules.TranslatorModuleImpl
 import okhttp3.OkHttpClient
 import okhttp3.internal.platform.Platform
@@ -13,6 +14,7 @@ import org.simpleframework.xml.convert.AnnotationStrategy
 import org.simpleframework.xml.core.Persister
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.jackson.JacksonConverterFactory
 import retrofit2.converter.simplexml.SimpleXmlConverterFactory
 import java.util.concurrent.TimeUnit
 
@@ -22,17 +24,17 @@ class ServerClient {
         private const val TIMEOUT_IN_SECONDS = 30L
     }
 
-    private val retrofitBuilder = Retrofit.Builder()
+    private val retrofitTranslator = Retrofit.Builder()
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .client(createHttpClient())
-
-    private val retrofitTranslator = retrofitBuilder
             .baseUrl(BuildConfig.TRANSLATOR_API_ENPOINT)
-            // .addConverterFactory(JacksonConverterFactory.create(mapper))
+            .addConverterFactory(JacksonConverterFactory.create(mapper))
             .build()
 
     @Suppress("DEPRECATION")
-    private val retrofitLyrics = retrofitBuilder
+    private val retrofitLyrics =  Retrofit.Builder()
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(createHttpClient())
             .baseUrl(BuildConfig.LYRICS_API_ENPOINT)
             .addConverterFactory(SimpleXmlConverterFactory
                     .createNonStrict(Persister(AnnotationStrategy())))
